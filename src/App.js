60e6b1d23e059ad1efb9e8213ad515315d9d9ece
James H. Schader <schadejh@dukes.jmu.edu>
2022-07-12 14:26:21 -0400

    finished the project
    -useState uses a string '' not an array []
    -setLetters takes only one argument, not two
    -.map preceded by .split('') to split the string into an array of single characters

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
 import './App.css';
-import {useState} from 'react';
+import { useState } from 'react';
 import { Row } from './Row';
 
-// const letters = [{letter: "f"} , {letter: "i"}, {letter: "g"}];
-
 function App() {
-  const [letters, setLetters] = useState([]);
+  const [letters, setLetters] = useState('');
 
   return (
     <div className="App">
       <input
         id="seedWord"
         type="text"
-        onChange={e => setLetters(e.target.value,letters)}
+        onChange={e => setLetters(e.target.value)}
       />
-      <span>{letters}</span>
       {
-        letters.map(letter =>
+        letters.split('').map(letter =>
           <Row
-            letter ={letter}
+            letter={letter}
           />
         )
       }
-
-      {/* <Row letter={letters.charAt(1)}/> */}
     </div>
   );
 }
